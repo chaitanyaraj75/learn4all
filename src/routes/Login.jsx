b484de5ebb5e2d49eaa9d7efcddd496bda3bb0a8
Nidhi Paswan <nidhipaswan397@gmai.com>
2025-07-29 18:15:35 +0530

    modified about and Login page

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,9 +1,32 @@
+// import { Helmet } from "react-helmet";
+// import React from "react";
+// import Navbar from "../component/Navbar";
+// import Footer from "../component/Footer";
 function Login(){
     return (
         <div>
         <h1>Login Page</h1>
         <p>Please enter your credentials to log in.</p>
-        {/* Add login form here */}
+        {/* Add login form here */
+        <div className="form-box">
+      <h2>Create an Account</h2>
+      <p>Free learning resources for all classes</p>
+      <input type="text" placeholder="Full Name" />
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Confirm Password" />
+      <p>Already have an account?</p>
+      <button className="primary-btn">Sign Up</button>
+      <div className="divider">OR</div>
+      <button className="google-btn">
+        <img
+          src={googleLogo}
+          alt="Google"
+          style={{ width: "20px", marginRight: "8px" }}
+        />
+        Login with Google
+      </button>
+    </div>
+        }
         </div>
     );
 }
